e55e752277cd0e23703f4557eb4bbab36670b7d2
blackdog70 <sbonisolo@gmail.com>
2016-04-10 21:15:48 +0200

    Added toggle and little refactoring

--- domuino/domuino/network.py
+++ domuino/domuino/network.py
@@ -1,6 +1,6 @@
 import os
 import time
-from threading import Thread
+from threading import Thread, Lock
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -21,29 +21,38 @@ class Network(Thread):
     def __init__(self):
         super(Network, self).__init__()
         self.outputs = {}
-        self.devices = {}
+        self._devices = {}
         Device = Base.metadata.tables['device']
         for device in session.query(Device).all():
             with Domuino(device.address, device.code, 115200) as d:
-                self.devices[device.code] = d
+                self._devices[device.code] = d
 
     #TODO: Need To Be Optimized
     def update(self):
-        for device in self.devices.itervalues():
+        for device in self._devices.itervalues():
             try:
+                network_lock.acquire()
                 err, res = device.get_outputs()
+                network_lock.release()
                 if not err:
-                    outputs = res.split('@')[0]
+                    outputs = res.split('|')[1]
                     if outputs != 'null':
                         self.outputs[device.code] = eval(outputs)
             except Exception as e:
                 print e
 
+    def pin_toggle(self, device_code, output_pin):
+        device = self._devices.get(int(device_code))
+        if device:
+            network_lock.acquire()
+            device.toggle(output_pin)
+            network_lock.release()
+
     def run(self):
         while True:
             self.update()
 
-
+network_lock = Lock()
 network = Network()
 network.start()
 
--- fab_addon_lights/fab_addon_lights/views.py
+++ fab_addon_lights/fab_addon_lights/views.py
@@ -11,23 +11,7 @@ class Dashboard(BaseView):
     default_view = "lights"
     template_folder = '/home/sebastiano/PycharmProjects/home/fab_addon_lights/fab_addon_lights/templates'
 
-    @expose('/lights/')
-    @has_access
-    def lights(self):
-        lights = []
-        for light in self.appbuilder.get_session.query(Pin).filter(Pin.type_id == 1).all():
-            value = None
-            outputs = network.outputs.get(light.device.code)
-            if outputs:
-                value = outputs[light.code]
-            lights.append({"name": light.name,
-                           "room": light.room,
-                           "value": value,
-                           "code": light.code})
-        return self.render_template('list_lights.html', lights=lights)
-
-    @expose('/get/', methods=('GET', 'POST'))
-    def get(self):
+    def list_lights(self):
         lights = []
         for light in self.appbuilder.get_session.query(Pin).filter(Pin.type_id == 1).all():
             value = None
@@ -38,6 +22,17 @@ class Dashboard(BaseView):
                            "room": str(light.room),
                            "value": value,
                            "code": light.code})
+        return lights
+
+    @expose('/lights/')
+    @has_access
+    def lights(self):
+        lights = self.list_lights()
+        return self.render_template('list_lights.html', lights=lights)
+
+    @expose('/get/', methods=('GET', 'POST'))
+    def get(self):
+        lights = self.list_lights()
         return jsonify({"data": lights})
 
     @expose('/set/', methods=('GET', 'POST'))
@@ -45,8 +40,6 @@ class Dashboard(BaseView):
         pin_name = request.form.get('data')
         if pin_name:
             device_code, pin_id = pin_name.split('_')
-            device = network.devices.get(int(device_code))
-            if device:
-                print device.toggle(int(pin_id))
+            network.pin_toggle(device_code, pin_id)
         print request.form.get('data')
         return jsonify(result={})

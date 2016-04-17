9ae5922be04eb906fafffae8dbe5d2d8e3818cdb
blackdog70 <sbonisolo@gmail.com>
2016-04-17 07:05:08 +0200

    Fixed commit for insert query and added init of function records

--- domuino/domuino/db_data.py
+++ domuino/domuino/db_data.py
@@ -1,21 +1,36 @@
 from db_session import session
 from automation import functions
 
+
 def init_pin_type():
-    session.execute("insert or replace into input_type values(:id, :name)" ,{"id": 1, "name": "Button"})
-    session.execute("insert or replace into input_type values(:id, :name)" ,{"id": 5, "name": "Dimmer"})
-    session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 1, "name": "Light"})
-    session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 5, "name": "Light dimmered"})
-    session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 10, "name": "Socket"})
-    session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 20, "name": "Balcony"})
+    # session.execute("insert or replace into input_type values(:id, :name)" ,{"id": 1, "name": "Button"})
+    # session.execute("insert or replace into input_type values(:id, :name)" ,{"id": 5, "name": "Dimmer"})
+    # session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 1, "name": "Light"})
+    # session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 5, "name": "Light dimmered"})
+    # session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 10, "name": "Socket"})
+    # session.execute("insert or replace into output_type values(:id, :name)" ,{"id": 20, "name": "Balcony"})
+    session.execute("insert or replace into pin_type values(:id, :name)" ,{"id": 0, "name": "Input"})
+    session.execute("insert or replace into pin_type values(:id, :name)" ,{"id": 1, "name": "Output"})
+    session.commit()
+
+def init_pin_function():
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 100, "name": "Button"})
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 105, "name": "Dimmer"})
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 200, "name": "Light"})
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 205, "name": "Light dimmered"})
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 210, "name": "Socket"})
+    session.execute("insert or replace into pin_function values(:id, :name)" ,{"id": 220, "name": "Balcony"})
+    session.commit()
 
 
 def init_room():
     session.execute("insert or replace into room values(:id, :name)" ,{"id": 100, "name": "Kitchen"})
     session.execute("insert or replace into room values(:id, :name)" ,{"id": 110, "name": "Living"})
     session.execute("insert or replace into room values(:id, :name)" ,{"id": 120, "name": "Bathroom"})
+    session.commit()
 
-def fill_scenery_type():
+
+def fill_function():
     try:
         session.execute("delete from function")
         for function in functions.iterkeys():
@@ -26,7 +41,9 @@ def fill_scenery_type():
         session.rollback()
         print e
 
-fill_scenery_type()
+
+fill_function()
+init_pin_function()
 init_pin_type()
 init_room()
 
